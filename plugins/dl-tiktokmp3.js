6b949f935719292963362fde7904d3aaf3f3ee56
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-27 14:21:10 -0400

    Actualizar dl-tiktokmp3.js

--- plugins/dl-tiktokmp3.js
+++ plugins/dl-tiktokmp3.js
@@ -1,14 +1,52 @@
-let { generateWAMessageFromContent, proto, prepareWAMessageMedia } = require("@whiskeysockets/baileys") 
-let handler = async (m, { conn }) => {
-var contact = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-"contactMessage": {
-"displayName": `Whatsapp Bot`,
-"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Khemii;Bot;;;\nFN:Whatsapp Bot\nTEL;type=Mobile;waid=13135550002:+62 838-9738-7848\nEND:VCARD",
+let axios = require('axios')
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "• *Example :* .onlinegpt Hello"
+    conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
+    try {
+        let result = await generate(text)
+        await m.reply(result.reply)
+    } catch (e) {
+        await m.reply(eror)
+    }
 }
-}), { userJid: m.chat, quoted: m })
-conn.relayMessage(m.chat, contact.message, { messageId: contact.key.id })
-}
-handler.tags = ['ai']
-handler.help = ['metaai']
-handler.command = ["metaai"]
+handler.help = ["onlinegpt *<text>*"]
+handler.tags = ["ai"];
+handler.command = /^(onlinegpt)$/i
 module.exports = handler
+
+/* New Line */
+async function generate(q) {
+    try {
+        const {
+            data
+        } = await axios(
+            `https://onlinegpt.org/wp-json/mwai-ui/v1/chats/submit`, {
+                method: "post",
+                data: {
+                    botId: "default",
+                    newMessage: q,
+                    stream: false,
+                },
+                headers: {
+                    Accept: "text/event-stream",
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return data;
+    } catch (err) {
+        console.log(err.response.data);
+        return err.response.data.message;
+    }
+}
