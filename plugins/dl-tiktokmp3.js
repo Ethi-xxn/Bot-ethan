6691684bfa5a1149ced02bedbb0cf367e960c4c3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-27 14:27:03 -0400

    Actualizar dl-tiktokmp3.js

--- plugins/dl-tiktokmp3.js
+++ plugins/dl-tiktokmp3.js
@@ -1,24 +1,25 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args, text, command, usedPrefix, isCreator, isPrems }) => {
-  if (!text) return conn.reply(m.chat, `• *Example :* ${usedPrefix}${command} Hello`, m)
-	conn.sendMessage(m.chat, {
-		react: {
-			text: '🕒',
-			key: m.key,
-		}
-	})
-  let res = `https://api.yanzbotz.my.id/api/tts/ttstiktok?text=${text}&id=id_001`
-  conn.sendFile(m.chat, res, `openaiv.mp3`, '', m, true);
+let handler = async (m, { conn }) => {
+    const messages = conn.chats[m.chat].messages;
+    const participantCounts = {};
+    Object.values(messages).forEach(({ key }) =>
+        participantCounts[key.participant] = (participantCounts[key.participant] || 0) + 1
+    );
+    const sortedData = Object.entries(participantCounts)
+        .sort((a, b) => b[1] - a[1]);
+    const totalM = sortedData.reduce((acc, [, total]) => acc + total, 0);
+    const totalPeople = sortedData.length;
+    const pesan = sortedData
+        .map(([jid, total], index) => `*${index + 1}.* ${jid.replace(/(\d+)@.+/, '@$1')}: *${total}* pesan`)
+        .join('\n');
+    await conn.reply(m.chat, `📊 *Total Pesan Terakhir*: *${totalM}* pesan dari *${totalPeople}* orang\n\n${pesan}`, m, { contextInfo: {
+                mentionedJid: sortedData.map(([jid]) => jid)
+            }
+           }
+           )
 }
-handler.help = ['ttstiktok *<teks>*']
-handler.tags = ['ai']
-handler.command = /^ttstiktok$/i
-handler.limit = true
-handler.premium = false
-module.exports = handler
+handler.help = ['totalpesan'];
+handler.tags = ['group'];
+handler.command = /^(totalpesan)$/i;
+handler.group = true;
 
 module.exports = handler;
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
