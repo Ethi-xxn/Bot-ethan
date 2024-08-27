4a08bd0f1466854b10c4904a7cc451c8ca7ba129
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-27 14:33:46 -0400

    Actualizar Prueba1.js

--- plugins/Prueba1.js
+++ plugins/Prueba1.js
@@ -1,21 +1,52 @@
-var fetch = require("node-fetch")
-
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `• *Example :* ${usedPrefix + command} loli kawai`, m)
-  conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-  try {
-  var kemii = await fetch(`https://api.lolhuman.xyz/api/pixiv?apikey=${global.lolkey}&query=${text}`)
-  var res = await kemii.json()
-  var name = m.sender
-  conn.sendFile(m.chat, res.result[0].image, '', done, m)
-  } catch (e) {
-    console.log(e);
-    m.reply(`Failed to search ${text}`);
-  }
+let fetch = require("node-fetch")
+let handler = async (m, {
+    conn,
+    isOwner,
+    usedPrefix,
+    command,
+    args
+}) => {
+    let query = "• *Example :* .vnmorse Kemii"
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw query
+    await conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
+    try {
+        let res = await Morse(text)
+        await conn.sendMessage(m.chat, {
+            audio: res,
+            seconds: fsizedoc,
+            ptt: true,
+            mimetype: "audio/mpeg",
+            fileName: "vn.mp3",
+            waveform: [100, 0, 100, 0, 100, 0, 100]
+        }, {
+            quoted: m
+        })
+    } catch (e) {
+        m.reply('Failed')
+    }
 }
+handler.help = ["vnmorse *<text>*"]
+handler.tags = ["internet"]
+handler.command = /^(vnmorse)$/i
+export default handler
 
-handler.help = ['pixiv'].map(v => v + ' *<teks>*')
-handler.command = /^(pixiv)$/i  
-handler.tags = ['internet']
-handler.limit = true
-export default handler;
+async function Morse(input) {
+    let res = await fetch('http://api.funtranslations.com/translate/morse/audio?text=' + input + '&speed=0&tone=0', {
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json'
+        }
+    });
+    let su = await res.json()
+    let wavUrl = su.contents.translated.audio
+    let buffer = Buffer.from(
+        wavUrl.split('base64,')[1],
+        'base64'
+    )
+    return buffer
+}
