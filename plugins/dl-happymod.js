d359665e43914468d1368efeb80239ce3f3ca71a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-27 14:00:11 -0400

    Actualizar plugins

---
+++ plugins/dl-happymod.js
@@ -0,0 +1,19 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn,  text, usedPrefix, command }) => {
+	if (!text) return conn.reply(m.chat, `[❗] *Penggunaan:* ${usedPrefix + command} <name>`, m)
+	conn.sendMessage(m.chat, { react: { text: "⏳", key: m.key } });
+	let kemii = await fetch(`https://api.zahwazein.xyz/webzone/happymod?query=${text}&apikey=${global.zein}`)
+	let res = await kemii.json()
+    let hasil = res.result.map((v, i) => `Name: ${v.name}\nLink: ${v.link}\n`).join('\n')
+    let v = res.result
+	await conn.sendFile(m.chat, res.result[0].icon, 'happy.jpg', `${hasil}`, m)
+   await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+}
+handler.help = ['happymod']
+handler.tags = ['downloader']
+
+handler.command = /^(happymod)$/i
+handler.premium = false
+
+module.exports = handler
