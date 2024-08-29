a3d970dcca6c3a0f1efbd40323cfdd9537faec9f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-29 12:56:29 -0400

    Actualizar game-acertijo-resp.js

--- plugins/game-acertijo-resp.js
+++ plugins/game-acertijo-resp.js
@@ -5,7 +5,7 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('✨️ Ese juego ya ha terminado!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
