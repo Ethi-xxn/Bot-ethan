b83c5c978ce7b0ba1713329e2033a67f7545d74d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-28 21:04:20 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -32,6 +32,6 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
 handler.help = ['bing <text>'];
 handler.tags = ['tools'];
-handler.command = /^(bing)$/i;
+handler.command = /^(bing5)$/i;
 
 export default handler;
