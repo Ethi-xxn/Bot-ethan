07697efc137f3e24e1aeae1fd967eb98a16b561e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-29 13:13:04 -0400

    Actualizar menu-help.js

--- plugins/menu-help.js
+++ plugins/menu-help.js
@@ -160,8 +160,15 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     let img10 = "https://i.ibb.co/WFX3GhG/file.jpg";
     let img11 = "https://i.ibb.co/WFX3GhG/file.jpg";
 
-    await conn.sendButtonMessages(m.chat, menu, txt, `𝗠𝗘𝗡𝗨𝗦 𝗚𝗘𝗡𝗘𝗦𝗜𝗦`, [vid, img, img2, img3, img4, img5, img6, img8, img9, img10, img11].getRandom(), listSections, fcontact);
-};
+/*    await conn.sendButtonMessages(m.chat, menu, txt, ``, [vid, img, img2, img3, img4, img5, img6, img8, img9, img10, img11].getRandom(), , fcontact);
+};*/
+    await conn.sendButtonMessages(m.chat, menu, txt, [
+[vid, img, img2, img3, img4, img5, img6, img8, img9, img10, img11].getRandom(), [
+], null, [
+[]
+],
+[["𝗠𝗘𝗡𝗨𝗦 𝗚𝗘𝗡𝗘𝗦𝗜𝗦", listSections]]
+]], m, fake)
 
 handler.command = ["menu", "help", "menú"];
 
