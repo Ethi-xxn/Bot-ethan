dd831a52ddf3c5eb5ba102fc7613a7b455708138
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-29 13:03:21 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text }) => {
     }
 };
 
-handler.command = handler.help = ['bing'];
+handler.command = handler.help = ['bing7'];
 handler.tags = ['ai'];
 handler.premium = true 
 
