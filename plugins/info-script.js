64d2cd299886bd3b86c57c9e23c383d5b92ce246
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-29 13:51:20 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let pp = 'https://i.ibb.co/YcdFyft/file.jpg'
+let pp = 'https://i.ibb.co/8g5yZr5/file.jpg'
 m.react('🤍')
 let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
