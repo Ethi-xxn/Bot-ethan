5eca3d2b6592af2e492b47a2843637200b642cec
Ethi-xxn <xnmarianamx@gmail.com>
2024-09-09 12:46:57 -0500

    Update info-instalar_bot.js

--- plugins/info-instalar_bot.js
+++ plugins/info-instalar_bot.js
@@ -11,7 +11,7 @@ termux-setup-storage
 
 apt update && apt upgrade && pkg install -y git nodejs ffmpeg imagemagick yarn
 
-git clone https://github.com/Angelito-OFC/GenesisBot-Pro && cd GenesisBot-MD
+https://github.com/Ethi-xxn/Bot-ethan.git
 
 yarn install
 
