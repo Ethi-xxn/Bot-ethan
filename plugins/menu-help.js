10c5945aabe23cc634ae0923423df53c63418a9c
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-15 16:12:53 -0300

    Actualizar menu-help.js

--- plugins/menu-help.js
+++ plugins/menu-help.js
@@ -46,7 +46,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     let totalreg = Object.keys(global.db.data.users).length;
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
 
-    m.react("🤍");
+    m.react("🍌");
     let menu = ``;
 
     let txt = ""
